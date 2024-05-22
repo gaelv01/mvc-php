94a8114dc61fcd8822db3dc69caf1296432423ce
Gael Vivas <gaelvn2020@outlook.com>
2024-05-22 15:09:32 -0600

    updated db

--- db/cecyteq.sql
+++ db/cecyteq.sql
@@ -1,49 +1,280 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.4.32-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win64
--- HeidiSQL Versión:             12.7.0.6850
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-05-2024 a las 23:02:53
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Volcando datos para la tabla cecyteq.administrativos: ~1 rows (aproximadamente)
+--
+-- Base de datos: `cecyteq`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrativos`
+--
+
+CREATE TABLE `administrativos` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `Apellido` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `administrativos`
+--
+
 INSERT INTO `administrativos` (`ID`, `Nombre`, `Apellido`) VALUES
-	(1, 'Edgar', 'Vivas');
+(1, 'Edgar', 'Vivas');
 
--- Volcando datos para la tabla cecyteq.alumnos: ~1 rows (aproximadamente)
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(50) DEFAULT NULL,
+  `Apellidos` varchar(50) DEFAULT NULL,
+  `Fecha_Nacimiento` date DEFAULT NULL,
+  `Semestre` tinyint(4) DEFAULT NULL,
+  `Carrera` varchar(30) DEFAULT NULL,
+  `IdGrupo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
 INSERT INTO `alumnos` (`ID`, `Nombre`, `Apellidos`, `Fecha_Nacimiento`, `Semestre`, `Carrera`, `IdGrupo`) VALUES
-	(2000, 'Gael', 'Vivas Nieto', '2004-06-20', 1, 'Programación', 'TPROG-AMBI');
+(2000, 'Gael', 'Vivas Nieto', '2004-06-20', 2, 'Programación', 'TPROG-AMBI'),
+(2001, 'Saul', 'Zapata Perez', '2004-01-01', 2, 'Programación', 'TPROG-AMBI');
 
--- Volcando datos para la tabla cecyteq.calificaciones: ~1 rows (aproximadamente)
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `calificaciones`
+--
+
+CREATE TABLE `calificaciones` (
+  `Registro` int(11) NOT NULL,
+  `ID_Alumno` int(11) NOT NULL,
+  `ID_Materia` int(11) NOT NULL,
+  `IdGrupo` varchar(50) NOT NULL,
+  `Calificacion` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `calificaciones`
+--
+
 INSERT INTO `calificaciones` (`Registro`, `ID_Alumno`, `ID_Materia`, `IdGrupo`, `Calificacion`) VALUES
-	(1, 2000, 2000, 'TPROG-AMBI', 10);
+(1, 2000, 2000, 'TPROG-AMBI', 10);
 
--- Volcando datos para la tabla cecyteq.grupos: ~1 rows (aproximadamente)
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupos`
+--
+
+CREATE TABLE `grupos` (
+  `IdGrupo` varchar(50) NOT NULL,
+  `ID_Materia` int(11) NOT NULL,
+  `ID_Profesor` int(11) NOT NULL,
+  `Semestre` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `grupos`
+--
+
 INSERT INTO `grupos` (`IdGrupo`, `ID_Materia`, `ID_Profesor`, `Semestre`) VALUES
-	('TPROG-AMBI', 2000, 1, 1);
+('TPROG-AMBI', 2000, 1, 1);
 
--- Volcando datos para la tabla cecyteq.materias: ~1 rows (aproximadamente)
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materias`
+--
+
+CREATE TABLE `materias` (
+  `ID` int(11) NOT NULL,
+  `Materia` varchar(50) NOT NULL,
+  `Semestre` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `materias`
+--
+
 INSERT INTO `materias` (`ID`, `Materia`, `Semestre`) VALUES
-	(2000, 'TIC\'s', 1);
+(2000, 'TIC\'s', 1);
 
--- Volcando datos para la tabla cecyteq.profesores: ~1 rows (aproximadamente)
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesores`
+--
+
+CREATE TABLE `profesores` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(50) DEFAULT NULL,
+  `Apellidos` varchar(50) DEFAULT NULL,
+  `Fecha_Nacimiento` date DEFAULT NULL,
+  `IdGrupo` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `profesores`
+--
+
 INSERT INTO `profesores` (`ID`, `Nombre`, `Apellidos`, `Fecha_Nacimiento`, `IdGrupo`) VALUES
-	(1, 'Juan Manuel', 'Estrada', '2024-05-21', 'TPROG-AMBI');
+(1, 'Juan Manuel', 'Estrada', '2024-05-21', 'TPROG-AMBI');
 
--- Volcando datos para la tabla cecyteq.usuarios: ~1 rows (aproximadamente)
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `IdUsuario` varchar(30) NOT NULL,
+  `Password` varchar(30) NOT NULL,
+  `Tipo` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
 INSERT INTO `usuarios` (`IdUsuario`, `Password`, `Tipo`) VALUES
-	('315297', 'gael123', '1');
+('315297', 'gael123', '1');
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrativos`
+--
+ALTER TABLE `administrativos`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_Grupo` (`IdGrupo`);
+
+--
+-- Indices de la tabla `calificaciones`
+--
+ALTER TABLE `calificaciones`
+  ADD PRIMARY KEY (`Registro`),
+  ADD KEY `FK_Alumno` (`ID_Alumno`),
+  ADD KEY `FK_Materias` (`ID_Materia`);
+
+--
+-- Indices de la tabla `grupos`
+--
+ALTER TABLE `grupos`
+  ADD PRIMARY KEY (`IdGrupo`),
+  ADD KEY `FK_Profesor` (`ID_Profesor`),
+  ADD KEY `FK_Materia` (`ID_Materia`);
+
+--
+-- Indices de la tabla `materias`
+--
+ALTER TABLE `materias`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_Grupos` (`IdGrupo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `administrativos`
+--
+ALTER TABLE `administrativos`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2003;
+
+--
+-- AUTO_INCREMENT de la tabla `calificaciones`
+--
+ALTER TABLE `calificaciones`
+  MODIFY `Registro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `materias`
+--
+ALTER TABLE `materias`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2001;
+
+--
+-- AUTO_INCREMENT de la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD CONSTRAINT `FK_Grupo` FOREIGN KEY (`IdGrupo`) REFERENCES `grupos` (`IdGrupo`);
+
+--
+-- Filtros para la tabla `calificaciones`
+--
+ALTER TABLE `calificaciones`
+  ADD CONSTRAINT `FK_Alumno` FOREIGN KEY (`ID_Alumno`) REFERENCES `alumnos` (`ID`),
+  ADD CONSTRAINT `FK_Materias` FOREIGN KEY (`ID_Materia`) REFERENCES `materias` (`ID`);
+
+--
+-- Filtros para la tabla `grupos`
+--
+ALTER TABLE `grupos`
+  ADD CONSTRAINT `FK_Materia` FOREIGN KEY (`ID_Materia`) REFERENCES `materias` (`ID`),
+  ADD CONSTRAINT `FK_Profesor` FOREIGN KEY (`ID_Profesor`) REFERENCES `profesores` (`ID`);
+
+--
+-- Filtros para la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  ADD CONSTRAINT `FK_Grupos` FOREIGN KEY (`IdGrupo`) REFERENCES `grupos` (`IdGrupo`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
