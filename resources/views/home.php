048a5c2788e0be2acbe1cc4643085c7c031e0767
Gael Vivas <gaelvn2020@outlook.com>
2024-05-24 21:06:03 -0600

    added js functions

--- resources/views/home.php
+++ resources/views/home.php
@@ -5,13 +5,12 @@
     <meta charset='UTF-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title></title>
+    <title>Inicio</title>
     <script src='https://kit.fontawesome.com/cf7ee57364.js' crossorigin='anonymous'></script>
     <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,600,0,0' />
-    <!-- <script type='text/javascript' language='JavaScript' src='../js/reloj.js'></script>
-    <script type='text/javascript' language='JavaScript' src='js/funciones.js'></script> -->
+    <script type='text/javascript' language='JavaScript' src='../js/reloj.js'></script>
+    <script type='text/javascript' language='JavaScript' src='js/funciones.js'></script>
     <link rel='stylesheet' href='css/styles.css'>
-
 </head>
 
 <body onload='mostrarTiempo();'>
@@ -20,7 +19,7 @@
         <header class="header">
             <img src="img/logo.png" alt="Logo" class="header_img">
             <p class="header_p">Colegio de Estudios Científicos y Tecnológicos del Estado de Querétaro</p>
-            <!-- <span id="tiempo"></span> -->
+            <span id="tiempo"></span>
         </header>
         <main class="main --default">
 
